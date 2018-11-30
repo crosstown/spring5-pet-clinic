5004870f2d1c866d3b1bb45d095fd5bf34b9bbde
F.S.(Crosstown) <royalplanet2009@gmail.com>
2018-11-29 23:11:05 -0500

    Adding Index Page and Controller. Closes #8

---
+++ pet-clinic-web/src/main/java/com/crosstown/spring5petclinic/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package com.crosstown.spring5petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author developer
+ * @date 2018-11-29
+ * @time 22:58
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/", "index", "index.html"})
+    public  String index(){
+        return "index";
+    }
+}
