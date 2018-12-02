481fe76c7c3d4823baa8da616458cf142952ac1f
F.S.(Crosstown) <royalplanet2009@gmail.com>
2018-12-02 01:24:56 -0500

    Refactored Map Implementation to abstract ID generation. Closes #23.

--- pet-clinic-data/src/main/java/com/crosstown/spring5petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/crosstown/spring5petclinic/services/map/AbstractMapService.java
@@ -1,18 +1,17 @@
 package com.crosstown.spring5petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.crosstown.spring5petclinic.model.BaseEntity;
+
+import java.util.*;
 
 /**
  * @author developer
  * @date 2018-11-29
  * @time 21:03
  */
-public class AbstractMapService<T, ID> {
+public class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -22,8 +21,19 @@ public class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
+
 
         return  object;
     }
@@ -35,5 +45,16 @@ public class AbstractMapService<T, ID> {
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
 
+    }
+
+    private long getNextId() {
+        Long nextId = null;
+
+        try{
+            nextId =  Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
