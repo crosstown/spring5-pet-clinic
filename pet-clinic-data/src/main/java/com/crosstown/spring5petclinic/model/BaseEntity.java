4a0a1c8d4d3bf0ebd7fb6a68b14e02fe59ea316a
F.S.(Crosstown) <royalplanet2009@gmail.com>
2018-12-03 13:05:35 -0500

    Updated base entity with JPA annotations. Closes #35.

--- pet-clinic-data/src/main/java/com/crosstown/spring5petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/crosstown/spring5petclinic/model/BaseEntity.java
@@ -1,5 +1,9 @@
 package com.crosstown.spring5petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
@@ -7,7 +11,10 @@ import java.io.Serializable;
  * @date 2018-11-29
  * @time 19:34
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
