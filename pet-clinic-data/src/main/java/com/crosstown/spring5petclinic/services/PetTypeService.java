fda32ecf110bf66b51e9c509e3127091ce718b19
F.S.(Crosstown) <royalplanet2009@gmail.com>
2018-12-03 00:58:53 -0500

    Added PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/com/crosstown/spring5petclinic/services/PetTypeService.java
@@ -0,0 +1,12 @@
+package com.crosstown.spring5petclinic.services;
+
+import com.crosstown.spring5petclinic.model.PetType;
+
+/**
+ * @author developer
+ * @date 2018-12-03
+ * @time 00:40
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
