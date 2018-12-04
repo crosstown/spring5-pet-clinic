55bc12044effd4dd0ef519517a446b2ea51dc634
F.S.(Crosstown) <royalplanet2009@gmail.com>
2018-12-04 02:08:38 -0500

    Implemented Map Impl for Visit Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/crosstown/spring5petclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package com.crosstown.spring5petclinic.services;
+
+import com.crosstown.spring5petclinic.model.Visit;
+
+/**
+ * @author developer
+ * @date 2018-12-04
+ * @time 01:54
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
